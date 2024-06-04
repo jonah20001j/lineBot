7550c7809111983b710060ce4ecd859711f3ee65
stanley <jonah20001j@gmail.com>
2024-06-04 14:36:46 +0800

    add: 新增 lineBot 傳接程式碼 - 新增 lineBot 傳接程式碼 - 新增 讀取 .env 環境變數 檔案

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
-from flask import Flask, request   # 載入 Flask
+import os
+import json # 載入 json 標準函式庫，處理回傳的資料格式
 
-# 建立 app 變數為 Flask 物件，__name__ 表示目前執行的程式
+from dotenv import load_dotenv
+
+from flask import Flask, request
+
+# 載入 LINE Message API 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# 獲取當前目錄
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print('current_dir:' + current_dir)
+
+# 構建上一層目錄的 .env 檔案路徑
+dotenv_path = os.path.join(current_dir, '../', '.env')
+print('dotenv_path:' + dotenv_path)
+
+# 加載 .env 檔案
+load_dotenv(dotenv_path)
+# 讀取環境變數
+channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN')
+line_authtoken = os.getenv('LINE_AUTHTOKEN')
+print('channel_access_token:' + channel_access_token)
+print('line_authtoken:' + line_authtoken)
+
 app = Flask(__name__)
 
-# 使用函式裝飾器，建立一個路由 ( Routes )，可針對主網域 / 發出請求
 @app.route("/", methods=['POST'])
-# 發出請求後會執行 home() 的函式
-def home():
-    # 執行函式後會回傳特定的網頁內容
-    return "<h1>hello world</h1>"
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    # print(body)
+    try:
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        # access_token = channel_access_token
+        # secret = line_authtoken
+        line_bot_api = LineBotApi(channel_access_token)      # 確認 token 是否正確
+        handler = WebhookHandler(line_authtoken)             # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
+        if type=='text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)                                       # 印出內容
+            reply = msg
+        else:
+            reply = '你傳的不是文字呦～'
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
+    except:
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                                              # 驗證 Webhook 使用，不能省略
 
-@app.route("/ok", methods=['GET'])
-def ok():
-    return "<h1>ok</h1>"
-
-@app.route("/yes")
-def yes():
-    return "<h1>yes</h1>"
-
-@app.route("/args")
-def home():
-    print(request.args)            # 使用 request.args
-    return "<h1>hello world</h1>"
-
-# 執行
-app.run(host="0.0.0.0", port=5555)
+if __name__ == "__main__":
+    app.run()
